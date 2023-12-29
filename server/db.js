b1724a713f90b6a6630465d0433dbc0fbf95575a
Mohammad Bakeer <mohammadbakeer320@MacBook-Pro-53.attlocal.net>
2023-12-29 14:55:46 -0600

    Database env change

--- server/db.js
+++ server/db.js
@@ -1,7 +1,9 @@
+// pool.js
 const { Pool } = require('pg');
+require('dotenv').config(); // Load environment variables from .env file
 
 const pool = new Pool({
-  connectionString: 'postgres://bakeer320:sKFGWcwlJvpIafaUNfgUOKflKrbGrcwO@dpg-cm6vebgcmk4c738r2vag-a/todo_mohammad',
+  connectionString: process.env.DATABASE_URL, // Use DATABASE_URL from .env
   // Other options, if needed
 });
 
